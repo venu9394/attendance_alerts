4a72d487ccdb4e52f539423a40030329df75d55d
VENU9394 <45553410+venu9394@users.noreply.github.com>
2019-10-28 14:45:00 +0530

    commit

--- BirthDayAlerts/src/com/hhcl/init/InitializeServlet.java
+++ BirthDayAlerts/src/com/hhcl/init/InitializeServlet.java
@@ -9,7 +9,7 @@ public class InitializeServlet extends HttpServlet {
  public void init() throws ServletException {
     
     try {
-        System.out.println("Execute Job ***VENU **************");
+        System.out.println("Execute Job ***VENU ****************");
         
         MyApp objPlugin = new MyApp();
         objPlugin.calljob();
